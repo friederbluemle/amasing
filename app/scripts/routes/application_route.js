b99f94071afedc13c5af33f44dcbe2a6bbd6f54e
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-06-08 17:16:45 -0700

    Add mock data for categories

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -2,6 +2,39 @@ Amasing.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['red', 'yellow', 'blue'];
+        return data.categories;
     }
 });
+
+var data = {
+    "status":"ok",
+    "categories": [
+        {
+            "name":"Automotive"
+        },
+        {
+            "name":"Baby & Children"
+        },
+        {
+            "name":"Beauty & Health"
+        },
+        {
+            "name":"Home & Garden"
+        },
+        {
+            "name":"Kids & Toys"
+        },
+        {
+            "name":"Clothing & Apparel"
+        },
+        {
+            "name":"Books"
+        },
+        {
+            "name":"Computers"
+        },
+        {
+            "name":"Electronics"
+        }
+    ]
+}
