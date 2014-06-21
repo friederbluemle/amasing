e03091045387c8130e3ce2dc4de58e5f638d72ee
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-06-21 00:05:08 -0700

    Return only details object for product route

--- app/scripts/routes/product.js
+++ app/scripts/routes/product.js
@@ -1,7 +1,7 @@
 Amasing.ProductRoute = Ember.Route.extend({
     model: function () {
         return Ember.$.getJSON('/scripts/api.php?method=getMock&file=product.json').then(function(data) {
-            return data;
+            return data.details;
         });
     }
 });
